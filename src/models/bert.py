0db3dd8e3205f703518dcaedd5e85103380cbbbc
Yiming Ren <z5383693@ad.unsw.edu.au>
2023-10-08 09:58:44 +1100

    add (bert):1

---
+++ src/models/bert.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+from transformers import BertModel, BertTokenizer, BertPreTrainedModel, AutoConfig
+from vit_model import MLP
+
+
+def _init_vit_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.trunc_normal_(m.weight, std=.02)
+        if isinstance(m, nn.Linear) and m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    
+
+class Bert(BertPreTrainedModel):
+    def __init__(self, 
+                 out_dim=768,
+                 mlp_ratio=4.,
+                 mlp_drop=0.,
+                 config = AutoConfig.from_pretrained('bert-base-uncased')):
+        super(Bert, self).__init__(config)
+        self.bert = BertModel(config)
+        self.num_features = self.embed_dim = config.hidden_size
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.mlp = MLP(in_features=self.embed_dim, hidden_features=int(self.embed_dim * mlp_ratio), act_layer=nn.GELU , drop_ratio=mlp_drop, out_features=out_dim)
+        self.post_init()
+        self.apply(_init_vit_weights)
+
+    def forward(self,x):
+        # [batch_size, max_len, embed_dim]
+        x = self.bert(**x)
+        # [batch_size, embed_dim]
+        x = x[:,0,:]
+        # [batch_size, out_dim]
+        x = self.mlp(x)
+        return x
+    
+
+        
